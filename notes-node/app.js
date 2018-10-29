f0e5732f2e3ac23c7e205cb2d4c7b5347cd74ccc
Dafne <ddolca@github.com>
2018-10-29 16:12:15 +0100

    Adding, saving, removing and reading content

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,26 +1,39 @@
+/*jshint esversion: 6 */
+
 console.log("starting app.js");
-
-const yargs = require('yargs');
-
+//Require notes.js to get edit data with functions from user input
 const notes = require('./notes.js');
 
-            const argv = yargs.argv;
+//Require Yargs.argv to get user input in CLI
+const yargs = require('yargs');
+const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-// console.log('Yargs: ', argv);
 
-if (command === 'add') {;
-    notes.addNote(argv.title, argv.body);
+console.log('Command: ', command);
+
+//Calls functions from notes.js, according to
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note Created');
+        notes.logNote(note);
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found.');
+    }
 } else if (command === 'remove') {
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved
+        ? 'Note Removed'
+        : 'Note was not removed / not found';
+    console.log(message);
 } else {
     console.log('command not recognized');
 }
-
-// Unused: fs.appendFile('greetings.txt', `Hello ${user.username} ! You are
-// ${notes.age} years old.`); const fs = require('fs'); const os =
-// require('os'); const _ = require('lodash');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,30 +1,79 @@
+/*jshint esversion: 6 */
 console.log('starting notes.js');
+const fs = require('fs');
 
-// console.log(module);
+var fetchNotes = () => {
+    //try catch. catches error if try is not executable
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
 
-// module.exports.addNote = () => {
-
-// }
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
 
 var addNote = (title, body) => {
-    console.log('Adding note: ', title, body)
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    //new array that will contain values that already exist
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    //adds notes to json file ONLY if notes are unique
+    if (duplicateNotes.length === 0) {
+        console.log("The note will be added.");
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(note));
+        saveNotes(notes);
+        return note;
+    }
+    else {
+        console.log("Note is already in the file, and will not be added.");
+    }
 };
 
 var getAll = () => {
-    console.log('Listing all notes: ')
+    console.log('Listing all notes: ');
 };
 
 var getNote = (title) => {
-    console.log('Reading note: ', title)
+    //1. fetching
+    var notes = fetchNotes();
+    //2. filtering
+    var filteredNotes = notes.filter((note) => note.title === title);
+    //3. return
+    console.log('Reading note: ', filteredNotes);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
-    console.log('Removing note: ', title)
+    //fetch notes
+    var notes = fetchNotes(); 
+
+    //filter notes, removing the one with title
+    var filteredNotes = notes.filter((note) => note.title !== title);
+
+    // save new notes array
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+    console.log('---');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
